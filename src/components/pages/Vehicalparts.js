0846d6fc34b0ea78303799d91ec1e309250e5687
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-12 08:07:25 +0530

    Added Vehicalparts.js

---
+++ src/components/pages/Vehicalparts.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import './Login.css';
+// import { useForm } from 'react-hook-form';
+// import CardItem from '../CardItem';
+
+// export default function Form() {
+
+//     const { register, handleSubmit, watch, formState: { errors } } = useForm()
+//     const onSubmit = data => console.log(data);
+    
+//   return (
+//             <ul className='cards__items'>
+//               <CardItem
+//                 src='images/car.png'
+//                 text='Vehicle Parts'
+//                 path='/vehicalparts'
+//               />
+//               </ul>        
+//   )
+// }
+
+
+

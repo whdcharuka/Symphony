e1402185ddd030811367971866ca426acac10fd1
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-15 21:05:43 +0530

    Added changes

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -14,37 +14,45 @@ export default function Form() {
                   <h2>Fill in the details</h2>
                   <hr></hr>
                   <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                    
                     <label>Brand</label>
                       <input type="text" className='d' {...register("brand")} placeholder='Brand' />
+                    
                     <label>Model</label>  
                       <input type="text" className='d' {...register("model")} placeholder='Model' />
+                    
                     <label>Tittle</label>
                       <input type="text" className='d' {...register("tittle")} placeholder='Tittle' />
+                    
                     <label>Description</label>  
                       <input type="text" className='d' {...register("description")} placeholder='Description' />
-                      
-                      <hr></hr>
+
+                    <hr></hr>
                     <label>Add photo</label> 
                     <div className="wrapper">
                     <input type="file" className="file"></input>
                     <input type="file" className="file"></input>
                     <input type="file" className="file"></input>
                     </div>
-                      <hr></hr>
+                    
+                    <hr></hr>
                     <h4>Contact details</h4>
                     <label>Name</label>
                       <input type="text" className='d' {...register("name")} placeholder='Name' />
+                    
                     <label>Email</label>
                       <input type="text" className='d' {...register("email")} placeholder='Email' />
+                    
                     <label>Phone Number</label>
                       <input type="text" className='d' {...register("number")} placeholder='Phone Number' />
                       {errors.mobile?.type === "required" && "Mobile Number is required"}
                       {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-                      <button className='btn'>Share Item</button>
+                      
+                    <button className='btn'>Share Item</button>
                   </form>
-            </div>
+              </div>
           </div>
-        </section>
+    </section>
         
   )
 }

7603a85bba30dd539541e682ddcefaee6d7848e8
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-16 22:06:02 +0530

    CardItem.js

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,6 @@ function CardItem(props) {
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
-              alt='Travel Image'
               src={props.src}
             />
           </figure>

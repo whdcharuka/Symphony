2b501bef5bbb0ae988f2e574f73f4d956d63e422
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-17 08:39:56 +0530

    Vp9.js

---
+++ src/components/pages/Vp9.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../App.css';
+import Categories from './Categories';
+import Vp9ItemView from './Vp9ItemView';
+import Description from './Description';
+
+function Home() {
+  return (
+    <>
+      <section>
+        <div className='container d_flex'>
+          <Categories />
+            <div className='photo'>
+               <Vp9ItemView />
+            </div>
+            <div className='description'>
+               <Description/>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;

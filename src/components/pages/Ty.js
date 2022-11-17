005a7b044238ed5de8e5ee6f65b2611c48303c44
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-17 08:38:16 +0530

    Ty.js

---
+++ src/components/pages/Ty.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../App.css';
+import Categories from './Categories';
+import ToysItemView from './ToysItemView';
+import Description from './Description';
+
+function Home() {
+  return (
+    <>
+      <section>
+        <div className='container d_flex'>
+          <Categories />
+            <div className='photo'>
+               <ToysItemView />
+            </div>
+            <div className='description'>
+               <Description/>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;

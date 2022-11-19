99f5e3ecd794788b70298edc4ab0ac597069eb14
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-19 06:46:39 +0530

    Vp10.js

---
+++ src/components/Vehicalparts/Vp10.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../App.css';
+import Categories from '../pages/Categories';
+import Vp10View from './Vp10View';
+import Description from '../pages/Description';
+
+function Home() {
+  return (
+    <>
+      <section>
+        <div className='container d_flex'>
+          <Categories />
+            <div className='photo'>
+               <Vp10View />
+            </div>
+            <div className='description'>
+               <Description/>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;

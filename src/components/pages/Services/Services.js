b097bd35422d83af25f96f3251bae93ae3cfa3b0
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-22 07:09:00 +0530

    Services.js

---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../App.css';
+import ServicesItem from './ServicesItem';
+import Footer from '../../Footer';
+
+export default function Products() {
+  return (
+    <>
+      <ServicesItem />
+      <Footer />
+    </>
+  );
+}

ddc2b60e8e425be0ef0981dda646edfa7f0f84c3
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-22 06:48:24 +0530

    About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
-import CardsProducts from '../CardsProducts';
+import AboutItem from './AboutItem';
+import Footer from '../Footer';
 
 export default function Products() {
   return (
     <>
-      <CardsProducts />
+      <AboutItem />
+      <Footer />
     </>
   );
 }

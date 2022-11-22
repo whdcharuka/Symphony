6dd817b79760bc8436377db45dcc851d3bbc30b9
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-22 07:08:50 +0530

    Contacts.js

---
+++ src/components/pages/Contact/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../App.css';
+import ContactItem from './ContactItem';
+import Footer from '../../Footer';
+
+export default function Products() {
+  return (
+    <>
+      <ContactItem />
+      <Footer />
+    </>
+  );
+}

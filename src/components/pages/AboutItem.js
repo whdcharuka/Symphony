e7e20977b193516b88e8670032f3cc3b58355d1f
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-22 06:49:59 +0530

    AboutItem.js

---
+++ src/components/pages/AboutItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../Cards.css';
+import CardItem from '../CardItem';
+
+function CardsProducts() {
+  return (
+    <div className='cards'>
+      <h1>About Us</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/.png'
+              text=''
+              path='/'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardsProducts;

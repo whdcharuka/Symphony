f9c0e2a81f91cdc3d4ee74037c5b424760e2db28
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-22 07:44:28 +0530

    Modified

--- src/components/HomeGarden/HomeGarden.js
+++ src/components/HomeGarden/HomeGarden.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../App.css';
 import Categories from '../pages/Categories';
 import HgItem from './HgItem';
-import Footer from '../Footer';
 
 function Home() {
   return (
@@ -14,7 +13,6 @@ function Home() {
                <HgItem />
             </div>
         </div>
-        <Footer />
       </section>
     </>
   );

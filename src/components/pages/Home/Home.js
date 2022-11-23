62c05c2a4f090762034ff2e49e40c6a63a474c23
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-23 19:39:47 +0530

    Home.js

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../App.css';
+import CardsHome from './CardsHome';
+
+function Home() {
+  return (
+    <>
+      <CardsHome />
+    </>
+  );
+}
+
+export default Home;

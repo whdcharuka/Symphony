33f6207515ff9b1870585f222e5936d6e70d2023
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-23 20:49:04 +0530

    ContactItem.js

--- src/components/pages/Contact/ContactItem.js
+++ src/components/pages/Contact/ContactItem.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import '../../Cards.css';
-import CardItem from '../../CardItem';
+import React from 'react'
+import '../Signup.css';
+import { useForm } from 'react-hook-form';
 
-function CardsProducts() {
+export default function Form() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+    
   return (
-    <div className='cards'>
-      <h1>Contact</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/.png'
-              text=''
-              path='/'
-            />
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-}
+    <section>
+          <div className="cont">
+              <div className="col-1">
+                  <h2>Contact Us</h2>
+                  <hr></hr>
+                  <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                    
+                    {/* <label>First Name</label> */}
+                      <input type="text" className='d' {...register("fname")} placeholder='First Name' />
+                    
+                    {/* <label>Last Name</label>   */}
+                      <input type="text" className='d' {...register("lname")} placeholder='Last Name' />               
+                    
+                    {/* <label>Email</label>   */}
+                      <input type="text" className='d' {...register("email")} placeholder='Email' />
+                    
+                    {/* <label>Phone Number</label> */}
+                      <input type="text" className='d' {...register("number")} placeholder='Phone Number' />
+                      {errors.mobile?.type === "required" && "Mobile Number is required"}
+                      {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
 
-export default CardsProducts;
+                    {/* <label>Message</label>   */}
+                      <input type="text" className='e' {...register("message")} placeholder='Message' />
+                      
+                    <button className='btn'>Submit</button>
+                  </form>
+              </div>
+          </div>
+    </section>      
+  )
+}

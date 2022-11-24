7a5573456294e368b3683dca03b53dc720a590b0
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-24 20:45:28 +0530

    Services.js

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -4,18 +4,21 @@ import ServicesItem from './ServicesItem';
 import Ser from './Ser';
 // import Categories from './Categories';
 // import CardsHome from './CardsHome';
+import Wrapper from "../../wrapper/Wrapper"
 
 export default function Products() {
   return (
     <>
     {/* <Categories /> */}
-      <div className='container d_flex'>
+      {/* <div className='container d_flex'> */}
         
-        <ServicesItem />
-            <div className='contact_box'>
+        {/* <ServicesItem /> */}
+        {/* < Wrapper /> */}
+            <div className='contact_box1'>
+            < Wrapper />
               <Ser />
             </div>
-        </div>
+        {/* </div> */}
         {/* <CardsHome /> */}
     </>
   );

c1bae248742c01c8d04b5ff61a50a3c813523a55
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-25 16:44:38 +0530

    Modified

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../../../App.css';
 import CardsHome from './CardsHome';
-// import NewArrivals from './NewArrivals/NewArrivals';
+import Wrapper from './HomeWrapper';
 
 function Home() {
   return (
     <>
       <CardsHome />
-      {/* <NewArrivals /> */}
+      <Wrapper />
     </>
   );
 }

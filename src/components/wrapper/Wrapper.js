62aac11db498ad003a1d208d41ec67d07beffe37
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-25 09:55:09 +0530

    Wrapper.js

--- src/components/wrapper/Wrapper.js
+++ src/components/wrapper/Wrapper.js
@@ -1,33 +1,46 @@
 import React from "react"
-import "./style.css"
+import "./Wrapper.css"
 
 const Wrapper = () => {
   const data = [
     {
       cover: <i class="fa fa-truck" aria-hidden="true"></i>,
       title: "Worldwide Delivery",
-      // decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
     },
     {
       cover: <i class="fa fa-archive" aria-hidden="true"></i>,
       title: "Quality Product",
-      // decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
     },
     {
       cover: <i class="fa fa-thumbs-up" aria-hidden="true"></i>,
       title: "Shop With Confidence ",
-      // decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
     },
     {
       cover: <i class="fa fa-headphones" aria-hidden="true"></i>,
       title: "24/7 Support ",
-      // decs: "We offer competitive prices on our 100 million plus product any range.",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
     },
   ]
   return (
     <>
-      <section className='wrapper background'>
-        <div className='container grid2'>
+      <section className='wrapper wbackground'>
+        <div className='wcontainer grid2'>
+          {data.map((val, index) => {
+            return (
+              <div className='wproduct' key={index}>
+                <div className='img wicon-circle'>
+                  <i>{val.cover}</i>
+                </div>
+                <h3>{val.title}</h3>
+                <p>{val.decs}</p>
+              </div>
+            )
+          })}
+        </div>
+        {/* <div className='container grid2'>
           {data.map((val, index) => {
             return (
               <div className='product' key={index}>
@@ -39,8 +52,9 @@ const Wrapper = () => {
               </div>
             )
           })}
-        </div>
+        </div> */}
       </section>
+      
     </>
   )
 }

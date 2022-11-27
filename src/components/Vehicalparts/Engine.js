48ce415ae98a13867871b3c496658acaa6f0831d
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 21:30:10 +0530

    Modified

--- src/components/Vehicalparts/Engine.js
+++ src/components/Vehicalparts/Engine.js
@@ -1,18 +1,46 @@
+// import React from 'react';
+// import '../../App.css';
+// import Categories from '../pages/Categories';
+// import VpCategories from './VpCategories';
+// import EngineItem from './EngineItem';
+
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <div className='container d_flex'>
+//           <Categories />
+//           <VpCategories />
+//             <div className='qwe'>
+//                <EngineItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
 import React from 'react';
 import '../../App.css';
 import Categories from '../pages/Categories';
 import VpCategories from './VpCategories';
-import EngineItem from './EngineItem';
+import VpItem from './VpItem';
 
 function Home() {
   return (
     <>
       <section>
-        <div className='container d_flex'>
-          <Categories />
+        <h1 className='qwe3'>Engine Items</h1>
+        <div className='container g_flex'>
+          {/* <Categories /> */}
           <VpCategories />
-            <div className='qwe'>
-               <EngineItem />
+          
+            <div className='qwe1'>
+               <VpItem />
+               {/* <Wrapper /> */}
             </div>
         </div>
       </section>

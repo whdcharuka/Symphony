a16fae749190313a0fa3261b27fa0b9dcd50bf25
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 21:28:17 +0530

    Modified

--- src/components/FashionBeauty/FashionBeauty.js
+++ src/components/FashionBeauty/FashionBeauty.js
@@ -1,16 +1,38 @@
+// import React from 'react';
+// import '../../App.css';
+// import Categories from '../pages/Categories';
+// import FbItem from './FbItem';
+// import FbCategories from './FbCategories';
+
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <div className='container d_flex'>
+//           <Categories />
+//           <FbCategories />
+//             <div className='qwe'>
+//                <FbItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import FbItem from './FbItem';
-import FbCategories from './FbCategories';
+import Home1 from "./Home1"
 
 function Home() {
   return (
     <>
       <section>
+      <Home1/>
         <div className='container d_flex'>
-          <Categories />
-          <FbCategories />
             <div className='qwe'>
                <FbItem />
             </div>
--- src/components/FashionBeauty/FbItem.js
+++ src/components/FashionBeauty/FbItem.js
@@ -1,75 +1,152 @@
-import React from 'react'
-import '../pages/Login.css';
-import { useForm } from 'react-hook-form';
-import CardItem from '../CardItem';
-import { Link } from 'react-router-dom';
+// import React from 'react'
+// import '../pages/Login.css';
+// import { useForm } from 'react-hook-form';
+// import CardItem from '../CardItem';
+// import { Link } from 'react-router-dom';
 
-export default function Form() {
+// export default function Form() {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
+//     const { register, handleSubmit, watch, formState: { errors } } = useForm()
+//     const onSubmit = data => console.log(data);
     
+//   return (
+//     <div className='cards'>
+//     <h6>FASHION & BEAUTY<Link to='/All'><button className='btn1'>All</button></Link></h6>
+//       <div className='cards__container'>
+//         <div className='cards__wrapper'>
+//           <ul className='cards__items'>
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb1'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb2'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb3'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb4'
+//             />
+//             {/* <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb5'
+//             /> */}
+//           </ul> 
+//           <ul className='cards__items'>
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb6'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb7'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb8'
+//             />
+//             <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb9'
+//             />
+//             {/* <CardItem
+//               src='images/FashionBeauty/.jpg'
+//               text=''
+//               path='/Fb10'
+//             /> */}
+//           </ul>       
+//           </div>
+//         </div>
+//       </div>
+//   );
+// }
+
+
+import React from "react"
+import "../pages/Wrapper.css"
+import { Link } from "react-router-dom"
+
+const Wrapper = () => {
+  const data = [
+    {
+      path:"./Vp1",
+      cateImg: "./images/vehicalparts/horn.jpg",
+      title: "Horn ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp2",
+      cateImg: "./images/vehicalparts/air-filter.jpg",
+      title: "Air filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp3",
+      cateImg: "./images/vehicalparts/fuel filter.jpg",
+      title: "Fuel filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp4",
+      cateImg: "./images/vehicalparts/oil filter.jpg",
+      title: "Oil filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+  ]
   return (
-    <div className='cards'>
-    <h6>FASHION & BEAUTY<Link to='/All'><button className='btn1'>All</button></Link></h6>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb1'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb2'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb3'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb4'
-            />
-            {/* <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb5'
-            /> */}
-          </ul> 
-          <ul className='cards__items'>
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb6'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb7'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb8'
-            />
-            <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb9'
-            />
-            {/* <CardItem
-              src='images/FashionBeauty/.jpg'
-              text=''
-              path='/Fb10'
-            /> */}
-          </ul>       
-          </div>
+    <>
+      <section className='wrapper wbackground'>
+        <div className='wcontainer1 grid2'>
+          {data.map((val, index) => {
+            return (
+              <div className='vpproduct' key={index}>
+                <div>
+                <img src={val.cateImg} alt='' />
+                </div>
+                <h6>{val.title}</h6>
+                <p>{val.brand}</p>
+                <p>{val.model}</p>
+                <p>{val.decs}</p>
+                <p>Whatsapp : {val.phone}</p>
+                <p>{val.email}</p>
+                <link rel = "canonical" href = "https://www.javatpoint.com/"  />    
+                <Link to={val.phone}><button className='btn--medium'>CONTACT</button></Link>
+              </div>
+            ) 
+          })}
         </div>
-      </div>
-  );
+      </section> 
+    </>
+  )
 }
+
+export default Wrapper

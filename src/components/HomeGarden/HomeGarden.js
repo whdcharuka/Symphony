3ea31b7b4df21289093fb7af24b0a715fe790bd4
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 18:53:16 +0530

    Modified

--- src/components/HomeGarden/HomeGarden.js
+++ src/components/HomeGarden/HomeGarden.js
@@ -1,16 +1,38 @@
+// import React from 'react';
+// import '../../App.css';
+// import Categories from '../pages/Categories';
+// import HgItem from './HgItem';
+// import HgCategories from './HgCategories';
+
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <div className='container d_flex'>
+//           <Categories />
+//           <HgCategories />
+//             <div className='qwe'>
+//                <HgItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import HgItem from './HgItem';
-import HgCategories from './HgCategories';
+import Home1 from "./Home1"
 
 function Home() {
   return (
     <>
       <section>
+      <Home1/>
         <div className='container d_flex'>
-          <Categories />
-          <HgCategories />
             <div className='qwe'>
                <HgItem />
             </div>

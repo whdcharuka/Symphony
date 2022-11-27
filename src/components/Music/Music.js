96d647bf9f2f275cafff2c513b1d37992a4329f9
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 18:51:31 +0530

    Modified

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,16 +1,39 @@
+// import React from 'react';
+// import '../../App.css';
+// import Categories from '../pages/Categories';
+// import MuItem from './MuItem';
+// import MuCategories from './MuCategories';
+
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <div className='container d_flex'>
+//           <Categories />
+//           <MuCategories />
+//             <div className='qwe'>
+//                <MuItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import MuItem from './MuItem';
-import MuCategories from './MuCategories';
+import Home1 from "./Home1"
 
 function Home() {
   return (
     <>
       <section>
+      <Home1/>
         <div className='container d_flex'>
-          <Categories />
-          <MuCategories />
             <div className='qwe'>
                <MuItem />
             </div>

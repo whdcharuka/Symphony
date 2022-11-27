08432d81b5259aacabfb3f6613e98313ea0d5308
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 21:29:30 +0530

    Modified

--- src/components/Gifts/Gifts.js
+++ src/components/Gifts/Gifts.js
@@ -1,16 +1,38 @@
+// import React from 'react';
+// import '../../App.css';
+// import Categories from '../pages/Categories';
+// import GiItem from './GiItem';
+// import GiCategories from './GiCategories';
+
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <div className='container d_flex'>
+//           <Categories />
+//           <GiCategories />
+//             <div className='qwe'>
+//                <GiItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import GiItem from './GiItem';
-import GiCategories from './GiCategories';
+import Home1 from "./Home1"
 
 function Home() {
   return (
     <>
       <section>
+      <Home1/>
         <div className='container d_flex'>
-          <Categories />
-          <GiCategories />
             <div className='qwe'>
                <GiItem />
             </div>

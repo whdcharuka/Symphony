77211b1ea88265ba578fde94f31f901e33600adf
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-27 18:49:20 +0530

    Modified

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import '../../../App.css';
-import ServicesItem from './ServicesItem';
-import Ser from './Ser';
-// import Categories from './Categories';
-// import CardsHome from './CardsHome';
-import Wrapper from "../../wrapper/Wrapper"
+import React from 'react'
+import '../Login.css';
+import { useForm } from 'react-hook-form';
+import Wrapper from '../../wrapper/Wrapper';
 
-export default function Products() {
+export default function Form() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+    
   return (
-    <>
-      <div className='container d_flex'>
-        <ServicesItem />
-            <div className='contact_box1'>
-              <Ser />
-            </div>
-      </div>
-      < Wrapper />
-    </>
+   <section>
+    <div className="mklp">
+      <h1>Our Services</h1>
+      <img src='images/Customer-Service.jpg' />
+      <Wrapper/>
+    </div>
+</section>    
+      
   );
 }
+
+
+
+// import React from 'react';
+// import '../../../App.css';
+// import ServicesItem from './ServicesItem';
+// import Ser from './Ser';
+// // import Categories from './Categories';
+// // import CardsHome from './CardsHome';
+// import Wrapper from "../../wrapper/Wrapper"
+
+// export default function Products() {
+//   return (
+//     <>
+//       {/* <div className='container d_flex'> */}
+//         {/* <ServicesItem /> */}
+//             {/* <div className='contact_box1'>
+//               <Ser />
+//             </div> */}
+//       {/* </div> */}
+//       <Ser />
+//       {/* < Wrapper /> */}
+//     </>
+//   );
+// }

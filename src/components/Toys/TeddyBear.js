8fa938ca05a06e6cd6fef1e72ceba49b9be682d9
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-28 08:04:32 +0530

    Modified

--- src/components/Toys/TeddyBear.js
+++ src/components/Toys/TeddyBear.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import TyCategories from './TyCategories';
 import TyItem from './TyItem';
 
@@ -10,12 +9,9 @@ function Home() {
       <section>
         <h1 className='qwe3'>Teddy Bear</h1>
         <div className='container g_flex'>
-          {/* <Categories /> */}
           <TyCategories />
-          
             <div className='qwe1'>
                <TyItem />
-               {/* <Wrapper /> */}
             </div>
         </div>
       </section>

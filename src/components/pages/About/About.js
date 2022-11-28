0dc464ed17c95404b282ded6341002ab7234de99
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-28 21:21:22 +0530

    Modified

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import '../../../App.css';
-import AboutItem from './AboutItem';
-import Abt from './Abt';
+import React from "react"
+import "../Wrapper.css"
 
-export default function Products() {
+const Wrapper = () => {
+  const data = [
+    {
+      cover: <i class="fa fa-truck" aria-hidden="true"></i>,
+      title: "About our Company",
+      decs: "SLT Symphony is one of the Largest Online sharing Marketplaces in Sri Lanka that offers perfect classified advertisements to build your own businesses.",
+      decs1: "This platform is uniquely designed to promote your advertisements and consumers can easily browse through all categories and locations.",
+    },
+    {
+      cover: <i class="fa fa-archive" aria-hidden="true"></i>,
+      title: "What we do",
+      decs: "Our main goal is to Share what we have and finding what we want through the app. SLT symphony is specialized in promoting your advertisements to millions of users.",
+      decs1: "We manually review at the advertisements before publishing in our platform in order to make sure the quality is up to our standards. Contact Us for more details.",
+    },
+  ]
   return (
     <>
-    <div className='container d_flex'>
-       <AboutItem />
-            <div className='contact_box'>
-              <Abt />
-            </div>
+      <section className='wrapper wbackground'>
+        <div className="mklp">
+          <h1>About Symphony</h1>
+            <img src='images/aboutus.jpg' />
         </div>
-      
+        <div className='wcontainer grid7'>
+          {data.map((val, index) => {
+            return (
+              <div className='wproduct' key={index}>
+                <h1 className="how3">{val.title}</h1>
+                  <p>{val.decs}</p>
+                  <p>{val.decs1}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section> 
     </>
-  );
+  )
 }
+
+export default Wrapper

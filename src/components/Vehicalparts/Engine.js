8c9fd6e5234f1cd425c2cb3b40e116eb35be20dd
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-28 21:23:42 +0530

    Modified

--- src/components/Vehicalparts/Engine.js
+++ src/components/Vehicalparts/Engine.js
@@ -1,31 +1,5 @@
-// import React from 'react';
-// import '../../App.css';
-// import Categories from '../pages/Categories';
-// import VpCategories from './VpCategories';
-// import EngineItem from './EngineItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <div className='container d_flex'>
-//           <Categories />
-//           <VpCategories />
-//             <div className='qwe'>
-//                <EngineItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from 'react';
 import '../../App.css';
-import Categories from '../pages/Categories';
 import VpCategories from './VpCategories';
 import VpItem from './VpItem';
 
@@ -35,12 +9,10 @@ function Home() {
       <section>
         <h1 className='qwe3'>Engine Items</h1>
         <div className='container g_flex'>
-          {/* <Categories /> */}
           <VpCategories />
           
             <div className='qwe1'>
                <VpItem />
-               {/* <Wrapper /> */}
             </div>
         </div>
       </section>

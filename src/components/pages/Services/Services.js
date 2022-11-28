9e8a5b50864627524e277c93c60f9fee2c8ec778
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-28 21:21:30 +0530

    Modified

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,46 +1,52 @@
-import React from 'react'
-import '../Login.css';
-import { useForm } from 'react-hook-form';
-import Wrapper from '../../wrapper/Wrapper';
+import React from "react"
+import "../../wrapper/Wrapper.css"
 
-export default function Form() {
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
-    
+const Wrapper = () => {
+  const data = [
+    {
+      cover: <i class="fa fa-truck" aria-hidden="true"></i>,
+      title: "Fast Delivery",
+      decs: "We provide lslandwide delivery within 4-7 days.",
+    },
+    {
+      cover: <i class="fa fa-archive" aria-hidden="true"></i>,
+      title: "Quality Product",
+      decs: "We sell genuine products with company authorized.",
+    },
+    {
+      cover: <i class="fa fa-thumbs-up" aria-hidden="true"></i>,
+      title: "Shop With Confidence ",
+      decs: "Confident and active online services.",
+    },
+    {
+      cover: <i class="fa fa-headphones" aria-hidden="true"></i>,
+      title: "24/7 Support ",
+      decs: "Call our hotlines during office hours for any questions or inquiries.",
+    },
+  ]
   return (
-   <section>
-    <div className="mklp">
-      <h1>Our Services</h1>
-      <img src='images/Customer-Service.jpg' />
-      <Wrapper/>
-    </div>
-</section>    
-      
-  );
+    <>
+      <section className='wrapper wbackground'>
+        <div className="mklp">
+          <h1>Our Services</h1>
+            <img src='images/Customer-Service.jpg' />
+        </div>
+        <div className='wcontainer grid2'>
+          {data.map((val, index) => {
+            return (
+              <div className='wproduct' key={index}>
+                <div className='img wicon-circle'>
+                  <i>{val.cover}</i>
+                </div>
+                <h3>{val.title}</h3>
+                <p>{val.decs}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section> 
+    </>
+  )
 }
 
-
-
-// import React from 'react';
-// import '../../../App.css';
-// import ServicesItem from './ServicesItem';
-// import Ser from './Ser';
-// // import Categories from './Categories';
-// // import CardsHome from './CardsHome';
-// import Wrapper from "../../wrapper/Wrapper"
-
-// export default function Products() {
-//   return (
-//     <>
-//       {/* <div className='container d_flex'> */}
-//         {/* <ServicesItem /> */}
-//             {/* <div className='contact_box1'>
-//               <Ser />
-//             </div> */}
-//       {/* </div> */}
-//       <Ser />
-//       {/* < Wrapper /> */}
-//     </>
-//   );
-// }
+export default Wrapper

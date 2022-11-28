fe5fb4d7c873863fe5b357762d1bf67427980c1b
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-28 21:21:52 +0530

    Modified

--- src/components/Vehicalparts/Sdata.js
+++ src/components/Vehicalparts/Sdata.js
@@ -5,23 +5,5 @@ const Sdata = [
     desc: "Select your Choice",
     cover: "./images/Categories/car.png",
   },
-  // {
-  //   id: 2,
-  //   title: "50% Off For Your First Shopping",
-  //   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-  //   cover: "./images/SlideCard/slide-2.png",
-  // },
-  // {
-  //   id: 3,
-  //   title: "50% Off For Your First Shopping",
-  //   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-  //   cover: "./images/SlideCard/slide-3.png",
-  // },
-  // {
-  //   id: 4,
-  //   title: "50% Off For Your First Shopping",
-  //   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-  //   cover: "./images/SlideCard/slide-4.png",
-  // },
 ]
 export default Sdata

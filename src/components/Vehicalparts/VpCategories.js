391286abf6a87c41502c21a0c4e0d1d8d5307a78
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-29 17:21:21 +0530

    Modified

--- src/components/Vehicalparts/VpCategories.js
+++ src/components/Vehicalparts/VpCategories.js
@@ -3,99 +3,24 @@ import { Link } from "react-router-dom"
 import '../pages/SubCategories.css';
 
 const Categories = () => {
-  const data = [
-    {
-      path:"/Engine",
-      cateName: "ENGINE",
-    },
-    {
-      path:"/Battery",
-      cateName: "BATTERY",
-    },
-    {
-      path:"/Wheel",
-      cateName: "WHEEL",
-    }, 
-    {
-      path:"Fuel Tank",
-      cateName: "FUEL TANK",
-    },
-    {
-      path:"/Windscreen",
-      cateName: "WINDSCREEN",
-    },
-  ]
+  
   return (
     <>
       <div className='vcategory'>
       <h3 className='types'>TYPES</h3>
-        {data.map((value, index) => {
-          return (
-            <Link to={value.path} >
-              <div className='box f_flex1' key={index}>
-              <i class="fa fa-square" aria-hidden="true"><span>{value.cateName}</span></i>
-              </div>
-            </Link>
-          )
-        })}
+        <div className='box '>
+          <ul>
+            <Link to="./Engine"><i class="fa fa-square" aria-hidden="true"><span>ENGINE</span></i></Link>
+            <Link to="./Battery"><i class="fa fa-square" aria-hidden="true"><span>BATERY</span></i></Link>
+            <Link to="./Wheel"><i class="fa fa-square" aria-hidden="true"><span>WHEEL</span></i></Link>
+            <Link to="./Fuel"><i class="fa fa-square" aria-hidden="true"><span>FUEL TANK</span></i></Link>
+            <Link to="./Screen"><i class="fa fa-square" aria-hidden="true"><span>WINDSCREEN</span></i></Link>
+          </ul>
+          </div>
       </div> 
     </>
   )
 }
 
 export default Categories
-
-// import React from "react"
-// import { Link } from "react-router-dom"
-// import '../pages/Categories.css';
-
-// const Categories = () => {
-//   const data = [
-//     {
-//       path:"/Engine",
-//       cateImg: "./images/Categories/engineering.png",
-//       cateName: "ENGINE",
-//     },
-//     {
-//       path:"/Battery",
-//       cateImg: "./images/Categories/accumulator.png",
-//       cateName: "BATTERY",
-//     },
-//     {
-//       path:"/Wheel",
-//       cateImg: "./images/Categories/truck-wheel.png",
-//       cateName: "WHEEL",
-//     }, 
-//     {
-//       path:"./Fueltank",
-//       cateImg: "./images/Categories/oil-tank.png",
-//       cateName: "FUEL TANK",
-//     },
-//     {
-//       path:"/windscreen",
-//       cateImg: "./images/Categories/windscreen.png",
-//       cateName: "WINDSCREEN",
-//     },
-//   ]
-
-//   return (
-//     <>
-//       <div className='subcategory'>
-//         {data.map((value, index) => {
-//           return (
-//             <Link to={value.path} className='abc'>
-//               <div className='box f_flex' key={index}>
-//                   <img src={value.cateImg} alt='' />
-//                   <span>{value.cateName}</span>   
-//               </div>
-//             </Link>
-//           )
-//         })}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Categories
-
 

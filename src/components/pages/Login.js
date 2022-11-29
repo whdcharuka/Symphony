162c1da71a95eace2a9e2568100e2b7b3aa4dea4
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-29 17:22:55 +0530

    Modified

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -10,27 +10,6 @@ export default function Form() {
     
   return (
     <section>
-        {/* <section>
-          <div className="register">
-              <div className="col-1">
-                  <h2>Get Started with MAKER</h2>
-                  <span>Getting started is easy</span>
-
-                  <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                      <input type="text" className='d' {...register("fullname")} placeholder='Full Name' />
-                      <input type="text" className='d' {...register("email")} placeholder='Email' />
-                      <input type="text" className='d' {...register("pwd")} placeholder='Password' />
-                      <input type="text" className='d' {...register("confirmpwd")} placeholder='Confirm Password' />
-                      {errors.mobile?.type === "required" && "Mobile Number is required"}
-                      {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-                      <button className='btn'>Create Account</button>
-                  </form>
-            </div>
-              <div className="col-2">
-                  <img src='images/students.jpg' />
-              </div>
-          </div>
-        </section> */}
         <section>
             <div className="register">
                 <div className="col-1">
@@ -43,6 +22,7 @@ export default function Form() {
                           {errors.mobile?.type === "required" && "Mobile Number is required"}
                           {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
                           <button className='btn'>Login</button>
+                          <span>Forgot Password?<Link to='/Reset' className='abc'>  Reset Password </Link></span>
                       </form>
 
                   </div>
@@ -54,7 +34,7 @@ export default function Form() {
 
           <div className="register">
                 <div className="col-3">
-                      <span>Login to your account<Link to='/register' className='abc'>  Register </Link></span>
+                      <span>Don't have an account?<Link to='/register' className='abc'>  Register </Link></span>
                   </div>
               </div>
         </section>

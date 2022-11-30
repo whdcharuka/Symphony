ee46f8c5c8527b06ccde6a6ec86542e12b97d4e3
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-30 08:20:39 +0530

    Modified

--- src/components/Animals/AnCategories.js
+++ src/components/Animals/AnCategories.js
@@ -1,49 +1,74 @@
+// import React from "react"
+// import { Link } from "react-router-dom"
+// import '../pages/Categories.css';
+
+// const Categories = () => {
+//   const data = [
+//     {
+//       path:"/Cat",
+//       cateName: "CATS",
+//     },
+//     {
+//       path:"/",
+//       cateName: "DOGS",
+//     },
+//     {
+//       path:"/",
+//       cateName: "FISH",
+//     }, 
+//     {
+//       path:"/ ",
+//       cateName: "PET FOODS",
+//     },
+//     {
+//       path:"/",
+//       cateName: "FARM ANIMALS",
+//     },
+//   ]
+
+//   return (
+//     <>
+//       <div className='vcategory'>
+//       <h3 className='types'>TYPES</h3>
+//         {data.map((value, index) => {
+//           return (
+//             <Link to={value.path} className='abc'>
+//               <div className='box f_flex1' key={index}>
+//               <i class="fa fa-square" aria-hidden="true"><span>{value.cateName}</span></i>
+//               </div>
+//             </Link>
+//           )
+//         })}
+//         </div> 
+//     </>
+//   )
+// }
+
+// export default Categories
+
+
 import React from "react"
 import { Link } from "react-router-dom"
-import '../pages/SubCategories.css';
+import '../pages/Categories.css';
 
 const Categories = () => {
-  const data = [
-    {
-      path:"/Cat",
-      cateName: "CATS",
-    },
-    {
-      path:"/",
-      cateName: "DOGS",
-    },
-    {
-      path:"/",
-      cateName: "FISH",
-    }, 
-    {
-      path:"/ ",
-      cateName: "PET FOODS",
-    },
-    {
-      path:"/",
-      cateName: "FARM ANIMALS",
-    },
-  ]
-
+  
   return (
     <>
       <div className='vcategory'>
       <h3 className='types'>TYPES</h3>
-        {data.map((value, index) => {
-          return (
-            <Link to={value.path} className='abc'>
-              <div className='box f_flex1' key={index}>
-              <i class="fa fa-square" aria-hidden="true"><span>{value.cateName}</span></i>
-              </div>
-            </Link>
-          )
-        })}
-        </div> 
+        <div className='box '>
+          <ul>
+            <Link to="./School"><i class="fa fa-square" aria-hidden="true"><span>CATS</span></i></Link>
+            <Link to="./Note"><i class="fa fa-square" aria-hidden="true"><span>PET FOODS</span></i></Link>
+            <Link to="./Water"><i class="fa fa-square" aria-hidden="true"><span>DOGS</span></i></Link>
+            <Link to="./Stoory"><i class="fa fa-square" aria-hidden="true"><span>FARM ANIMALS</span></i></Link>
+            <Link to="./Stationary"><i class="fa fa-square" aria-hidden="true"><span>FISH</span></i></Link>
+          </ul>
+          </div>
+      </div> 
     </>
   )
 }
 
 export default Categories
-
-

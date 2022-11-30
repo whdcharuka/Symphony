a32e8aced5927b7467aae7d3d0537b03c87a79aa
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-11-30 08:20:53 +0530

    Modified

--- src/components/ElectronicDevices/ElCategories.js
+++ src/components/ElectronicDevices/ElCategories.js
@@ -1,49 +1,74 @@
+// import React from "react"
+// import { Link } from "react-router-dom"
+// import '../pages/Categories.css';
+
+// const Categories = () => {
+//   const data = [
+//     {
+//       path:"/Mobile",
+//       cateName: "MOBILE PHONES",
+//     },
+//     {
+//       path:"/",
+//       cateName: "TELEVISIONS",
+//     },
+//     {
+//       path:"/",
+//       cateName: "COMPUERS",
+//     }, 
+//     {
+//       path:"/ ",
+//       cateName: "AIR CONDITIONS",
+//     },
+//     {
+//       path:"/",
+//       cateName: "GAMING CONSOLES",
+//     },
+//   ]
+
+//   return (
+//     <>
+//       <div className='vcategory'>
+//       <h3 className='types'>TYPES</h3>
+//         {data.map((value, index) => {
+//           return (
+//             <Link to={value.path} className='abc'>
+//               <div className='box f_flex1' key={index}>
+//               <i class="fa fa-square" aria-hidden="true"><span>{value.cateName}</span></i>
+//               </div>
+//             </Link>
+//           )
+//         })}
+//         </div> 
+//     </>
+//   )
+// }
+
+// export default Categories
+
+
 import React from "react"
 import { Link } from "react-router-dom"
-import '../pages/SubCategories.css';
+import '../pages/Categories.css';
 
 const Categories = () => {
-  const data = [
-    {
-      path:"/Mobile",
-      cateName: "MOBILE PHONES",
-    },
-    {
-      path:"/",
-      cateName: "TELEVISIONS",
-    },
-    {
-      path:"/",
-      cateName: "COMPUERS",
-    }, 
-    {
-      path:"/ ",
-      cateName: "AIR CONDITIONS",
-    },
-    {
-      path:"/",
-      cateName: "GAMING CONSOLES",
-    },
-  ]
-
+  
   return (
     <>
       <div className='vcategory'>
       <h3 className='types'>TYPES</h3>
-        {data.map((value, index) => {
-          return (
-            <Link to={value.path} className='abc'>
-              <div className='box f_flex1' key={index}>
-              <i class="fa fa-square" aria-hidden="true"><span>{value.cateName}</span></i>
-              </div>
-            </Link>
-          )
-        })}
-        </div> 
+        <div className='box '>
+          <ul>
+            <Link to="./Phone"><i class="fa fa-square" aria-hidden="true"><span>MOBILE PHONES</span></i></Link>
+            <Link to="./Tele"><i class="fa fa-square" aria-hidden="true"><span>TELEVISIONS</span></i></Link>
+            <Link to="./Computer"><i class="fa fa-square" aria-hidden="true"><span>COMPUERS</span></i></Link>
+            <Link to="./Air"><i class="fa fa-square" aria-hidden="true"><span>AIR CONDITIONS</span></i></Link>
+            <Link to="./Game"><i class="fa fa-square" aria-hidden="true"><span>GAMING CONSOLES</span></i></Link>
+          </ul>
+          </div>
+      </div> 
     </>
   )
 }
 
 export default Categories
-
-

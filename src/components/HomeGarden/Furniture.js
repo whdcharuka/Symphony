c76a87ef76bf9175882689d1e44ee76b1e794cf6
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-01 07:09:28 +0530

    modified

--- src/components/HomeGarden/Furniture.js
+++ src/components/HomeGarden/Furniture.js
@@ -1,23 +1,104 @@
-import React from 'react';
-import '../../App.css';
-import HgCategories from './HgCategories';
-import HgItem from './HgItem';
+// import React from 'react';
+// import '../../App.css';
+// import HgCategories from './HgCategories';
+// import HgItem from './HgItem';
 
-function Home() {
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <h1 className='qwe3'>Furniture</h1>
+//         <div className='container g_flex'>
+//           <HgCategories />
+          
+//             <div className='qwe1'>
+//                <HgItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
+import React from "react"
+import "../pages/Wrapper.css"
+import '../../App.css';
+import { Link } from "react-router-dom"
+import HgCategories from "./HgCategories"
+
+const Wrapper = () => {
+  const data = [
+    {
+      path:"./Vp1",
+      cateImg: "./images/HomeGarden/1.jpg",
+      title: "Horn ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp2",
+      cateImg: "./images/HomeGarden/2.jpg",
+      title: "Air filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp3",
+      cateImg: "./images/HomeGarden/3.jpg",
+      title: "Fuel filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp4",
+      cateImg: "./images/HomeGarden/4.jpg",
+      title: "Oil filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+  ]
   return (
     <>
-      <section>
-        <h1 className='qwe3'>Furniture</h1>
-        <div className='container g_flex'>
-          <HgCategories />
-          
-            <div className='qwe1'>
-               <HgItem />
-            </div>
+    <h1 className='qwe3'>Furniture</h1>
+      <HgCategories/>
+      <section className='wrapper wbackground'>
+        <div className='wcontainer1 grid9'>
+          {data.map((val, index) => {
+            return (
+              <div className='vpproduct' key={index}>
+                <div>
+                <img src={val.cateImg} alt='' />
+                </div>
+                <h2>{val.title}</h2>
+                <p>{val.brand}</p>
+                <p>{val.model}</p>
+                <p>{val.decs}</p>
+                <p>Whatsapp : {val.phone}</p>
+                <p>{val.email}</p>
+                <p><link rel = "canonical" href = "https://www.javatpoint.com/"  />    
+                   <Link to={val.phone}><button className='btn--medium'>CONTACT</button></Link></p>
+              </div>
+            ) 
+          })}
         </div>
-      </section>
+      </section> 
     </>
-  );
+  )
 }
 
-export default Home;
+export default Wrapper
--- src/components/HomeGarden/HomeGarden.js
+++ src/components/HomeGarden/HomeGarden.js
@@ -1,45 +1,18 @@
-// import React from 'react';
-// import '../../App.css';
-// import Categories from '../pages/Categories';
-// import HgItem from './HgItem';
-// import HgCategories from './HgCategories';
+import React from "react"
+import Categories from "../pages/Categories"
+import SliderHome from "./Slider"
 
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <div className='container d_flex'>
-//           <Categories />
-//           <HgCategories />
-//             <div className='qwe'>
-//                <HgItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-import React from 'react';
-import '../../App.css';
-import HgItem from './HgItem';
-import Home1 from "./Home1"
-
-function Home() {
+const Home = () => {
   return (
     <>
-      <section>
-      <Home1/>
+      <section className='home'>
+      <Categories />
         <div className='container d_flex'>
-            <div className='qwe'>
-               <HgItem />
-            </div>
+          <SliderHome />
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home

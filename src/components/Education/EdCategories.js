de804242e6a22c12d9ee74ba26c945cf2ec90ab6
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-01 07:06:18 +0530

    modified

--- src/components/Education/EdCategories.js
+++ src/components/Education/EdCategories.js
@@ -64,10 +64,10 @@ const Categories = () => {
         <div className='box '>
           <ul>
             <Link to="./School"><i class="fa fa-square" aria-hidden="true"><span>SCHOOL BAGS</span></i></Link>
-            <Link to="./Water"><i class="fa fa-square" aria-hidden="true"><span>WATER BOTTLES</span></i></Link>
-            <Link to="./Stationary"><i class="fa fa-square" aria-hidden="true"><span>STATIONARIES</span></i></Link>
+            <Link to="./Bottle"><i class="fa fa-square" aria-hidden="true"><span>WATER BOTTLES</span></i></Link>
+            <Link to="./Stat"><i class="fa fa-square" aria-hidden="true"><span>STATIONARIES</span></i></Link>
             <Link to="./Note"><i class="fa fa-square" aria-hidden="true"><span>NOTE BOOKS</span></i></Link>
-            <Link to="./Stoory"><i class="fa fa-square" aria-hidden="true"><span>STORY BOOKS</span></i></Link>
+            <Link to="./Story"><i class="fa fa-square" aria-hidden="true"><span>STORY BOOKS</span></i></Link>
           </ul>
           </div>
       </div> 
--- src/components/Education/School.js
+++ src/components/Education/School.js
@@ -1,23 +1,103 @@
-import React from 'react';
-import '../../App.css';
-import EdCategories from './EdCategories';
-import EdItem from './EdItem';
+// import React from 'react';
+// import '../../App.css';
+// import EdCategories from './EdCategories';
+// import EdItem from './EdItem';
 
-function Home() {
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <h1 className='qwe3'>School Bags</h1>
+//         <div className='container g_flex'>
+//           <EdCategories />
+          
+//             <div className='qwe1'>
+//                <EdItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+import React from "react"
+import "../pages/Wrapper.css"
+import '../../App.css';
+import { Link } from "react-router-dom"
+import EdCategories from "./EdCategories"
+
+const Wrapper = () => {
+  const data = [
+    {
+      path:"./Vp1",
+      cateImg: "./images/Education/1.jpg",
+      title: "Horn ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp2",
+      cateImg: "./images/Education/2.jpg",
+      title: "Air filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp3",
+      cateImg: "./images/Education/3.jpg",
+      title: "Fuel filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp4",
+      cateImg: "./images/Education/4.jpg",
+      title: "Oil filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+  ]
   return (
     <>
-      <section>
-        <h1 className='qwe3'>School Bags</h1>
-        <div className='container g_flex'>
-          <EdCategories />
-          
-            <div className='qwe1'>
-               <EdItem />
-            </div>
+    <h1 className='qwe3'>Wate Bottles</h1>
+      <EdCategories/>
+      <section className='wrapper wbackground'>
+        <div className='wcontainer1 grid9'>
+          {data.map((val, index) => {
+            return (
+              <div className='vpproduct' key={index}>
+                <div>
+                <img src={val.cateImg} alt='' />
+                </div>
+                <h2>{val.title}</h2>
+                <p>{val.brand}</p>
+                <p>{val.model}</p>
+                <p>{val.decs}</p>
+                <p>Whatsapp : {val.phone}</p>
+                <p>{val.email}</p>
+                <p><link rel = "canonical" href = "https://www.javatpoint.com/"  />    
+                   <Link to={val.phone}><button className='btn--medium'>CONTACT</button></Link></p>
+              </div>
+            ) 
+          })}
         </div>
-      </section>
+      </section> 
     </>
-  );
+  )
 }
 
-export default Home;
+export default Wrapper

6f742ecce8d0b3963e990294deaf83aa0a28fc66
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-01 07:07:43 +0530

    modified

--- src/components/Foods/Beverage.js
+++ src/components/Foods/Beverage.js
@@ -1,23 +1,104 @@
-import React from 'react';
-import '../../App.css';
-import FdCategories from './FdCategories';
-import FdItem from './FdItem';
+// import React from 'react';
+// import '../../App.css';
+// import FdCategories from './FdCategories';
+// import FdItem from './FdItem';
 
-function Home() {
+// function Home() {
+//   return (
+//     <>
+//       <section>
+//         <h1 className='qwe3'>Beverages</h1>
+//         <div className='container g_flex'>
+//           <FdCategories />
+          
+//             <div className='qwe1'>
+//                <FdItem />
+//             </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
+import React from "react"
+import "../pages/Wrapper.css"
+import '../../App.css';
+import { Link } from "react-router-dom"
+import FdCategories from "./FdCategories"
+
+const Wrapper = () => {
+  const data = [
+    {
+      path:"./Vp1",
+      cateImg: "./images/Foods/1.jpg",
+      title: "Horn ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp2",
+      cateImg: "./images/Foods/2.jpg",
+      title: "Air filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp3",
+      cateImg: "./images/Foods/3.jpg",
+      title: "Fuel filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+    {
+      path:"./Vp4",
+      cateImg: "./images/Foods/4.jpg",
+      title: "Oil filter ",
+      brand:"Apple",
+      model:"single",
+      decs: "You can use decorate your table Table suitable",
+      phone:"021548936",
+      email:"ergset@gmail.com",
+    },
+  ]
   return (
     <>
-      <section>
-        <h1 className='qwe3'>Beverages</h1>
-        <div className='container g_flex'>
-          <FdCategories />
-          
-            <div className='qwe1'>
-               <FdItem />
-            </div>
+    <h1 className='qwe3'>Cakes</h1>
+      <FdCategories/>
+      <section className='wrapper wbackground'>
+        <div className='wcontainer1 grid9'>
+          {data.map((val, index) => {
+            return (
+              <div className='vpproduct' key={index}>
+                <div>
+                <img src={val.cateImg} alt='' />
+                </div>
+                <h2>{val.title}</h2>
+                <p>{val.brand}</p>
+                <p>{val.model}</p>
+                <p>{val.decs}</p>
+                <p>Whatsapp : {val.phone}</p>
+                <p>{val.email}</p>
+                <p><link rel = "canonical" href = "https://www.javatpoint.com/"  />    
+                   <Link to={val.phone}><button className='btn--medium'>CONTACT</button></Link></p>
+              </div>
+            ) 
+          })}
         </div>
-      </section>
+      </section> 
     </>
-  );
+  )
 }
 
-export default Home;
+export default Wrapper

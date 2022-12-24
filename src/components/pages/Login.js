a2659e3ec16106924e6cb4ffd31d927e5bec2996
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-24 20:57:17 +0530

    modified

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -40,3 +40,79 @@ export default function Form() {
         </section>
   )
 }
+
+
+
+// import React from 'react';
+// import { Form, Button } from 'semantic-ui-react';
+// import { useForm } from "react-hook-form";
+
+// import './Login.css';
+// import { Link } from 'react-router-dom';
+
+// export default function FormValidation() {
+//     const { register, handleSubmit, formState: { errors } } = useForm();
+//     const onSubmit = (data) => {
+//         console.log(data);
+//     }
+//     return (
+//         <div>
+//             <div className="register">
+//                <div className="col-1">
+//                   <h2>Get Started with MAKER</h2>
+//                   <span>Getting started is easy</span>
+//             <Form onSubmit={handleSubmit(onSubmit)}>
+//                 {/* <Form.Field>
+//                     <input
+//                         placeholder='First Name'
+//                         type="text"
+//                         {...register("firstName", { required: true, maxLength: 10 })}
+//                     />
+//                 </Form.Field>
+//                 {errors.firstName && <p>Please check the First Name</p>}
+//                 <Form.Field>
+//                     <input
+//                         placeholder='Last Name'
+//                         type="text"
+//                         {...register("lastName", { required: true, maxLength: 10 })}
+//                     />
+//                 </Form.Field>
+//                 {errors.lastName && <p>Please check the Last Name</p>} */}
+//                 <Form.Field>
+//                     <input
+//                         placeholder='Email'
+//                         type="email"
+//                         {...register("email",
+//                             {
+//                                 required: true,
+//                                 pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//                             })}
+//                     />
+//                 </Form.Field>
+//                 {errors.email && <p>Please check the Email</p>}
+//                 <Form.Field>
+//                     <input
+//                         placeholder='Password'
+//                         type="password"
+//                         {...register("password", {
+//                             required: true,
+//                             pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+//                         })}
+//                     />
+//                 </Form.Field>
+//                 {errors.password && <p>Please check the Password</p>}
+//                 <Button type='submit'>Submit</Button>
+//             </Form>
+//         </div>
+//         </div>
+//         <div className="register">
+//                 <div className="col-3">
+//                       <span>Login to your account<Link to='/register' className='abc'>  Register </Link></span>
+//                   </div>
+//               </div>
+//               {/* <div className="col-2">
+//                    <img src='images/istock.jpg' />
+//                </div> */}
+//            </div>
+//     )
+// }

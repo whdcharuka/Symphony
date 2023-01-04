8930f4188f98f7c65da05800917de5f68b039c47
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-04 13:48:39 +0530

    modified

--- src/components/Gifts/Christmas.js
+++ src/components/Gifts/Christmas.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import GiCategories from './GiCategories';
-// import GiItem from './GiItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Christmas</h1>
-//         <div className='container g_flex'>
-//           <GiCategories />
-          
-//             <div className='qwe1'>
-//                <GiItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

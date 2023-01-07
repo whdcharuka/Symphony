c5734aadf77132f25fbc86e8a3ee60602d2a4425
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-07 14:11:04 +0530

    modified

--- src/components/Animals/Cat.js
+++ src/components/Animals/Cat.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import AnCategories from './AnCategories';
-// import AnItem from './AnItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Cats</h1>
-//         <div className='container g_flex'>
-//           <AnCategories />
-//             <div className='qwe1'>
-//                <AnItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

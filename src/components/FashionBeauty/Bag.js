5d5254ed170a7c2460621a721b841631817c5c12
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:20:20 +0530

    modified

--- src/components/FashionBeauty/Bag.js
+++ src/components/FashionBeauty/Bag.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import FbCategories from './FbCategories';
-// import FbItem from './FbItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Bags</h1>
-//         <div className='container g_flex'>
-//           <FbCategories />
-          
-//             <div className='qwe1'>
-//                <FbItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

cd7ee11ef5ae7870175313f98a10617bc60ae524
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:21:06 +0530

    modified

--- src/components/HomeGarden/Furniture.js
+++ src/components/HomeGarden/Furniture.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import HgCategories from './HgCategories';
-// import HgItem from './HgItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Furniture</h1>
-//         <div className='container g_flex'>
-//           <HgCategories />
-          
-//             <div className='qwe1'>
-//                <HgItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

0ec372efbdadfb09c3825bab36822d57387d9da0
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:20:12 +0530

    modified

--- src/components/ElectronicDevices/Mobile.js
+++ src/components/ElectronicDevices/Mobile.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import Categories from '../pages/Categories';
-// import ElCategories from './ElCategories';
-// import ElItem from './ElItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Mobile</h1>
-//         <div className='container g_flex'>
-//           <ElCategories />
-//             <div className='qwe1'>
-//                <ElItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

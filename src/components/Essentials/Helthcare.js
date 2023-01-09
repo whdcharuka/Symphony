812b0ffad7ff3003a81344c8e7b76aad85bd811d
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:20:16 +0530

    modified

--- src/components/Essentials/Helthcare.js
+++ src/components/Essentials/Helthcare.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import EsCategories from './EsCategories';
-// import EsItem from './EsItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Helthcare</h1>
-//         <div className='container g_flex'>
-//           <EsCategories />
-          
-//             <div className='qwe1'>
-//                <EsItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

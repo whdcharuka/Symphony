591c9112bd0003371c698474050e90c70976b824
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:21:19 +0530

    modified

--- src/components/Music/Violin.js
+++ src/components/Music/Violin.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import MuCategories from './MuCategories';
-// import MuItem from './MuItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Violine</h1>
-//         <div className='container g_flex'>
-//           <MuCategories />
-//             <div className='qwe1'>
-//                <MuItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

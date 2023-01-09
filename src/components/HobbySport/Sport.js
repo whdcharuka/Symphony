c17389e034ac28da563c3c3ab5d6d803c4c183a5
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:20:48 +0530

    modified

--- src/components/HobbySport/Sport.js
+++ src/components/HobbySport/Sport.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import HsCategories from './HsCategories';
-// import HsItem from './HsItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>Sports</h1>
-//         <div className='container g_flex'>
-//           <HsCategories />
-          
-//             <div className='qwe1'>
-//                <HsItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

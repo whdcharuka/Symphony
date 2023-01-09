712ddea867e59b098bd18f9618b7d1eace4ab2fd
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-09 09:20:02 +0530

    modified

--- src/components/Education/School.js
+++ src/components/Education/School.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import '../../App.css';
-// import EdCategories from './EdCategories';
-// import EdItem from './EdItem';
-
-// function Home() {
-//   return (
-//     <>
-//       <section>
-//         <h1 className='qwe3'>School Bags</h1>
-//         <div className='container g_flex'>
-//           <EdCategories />
-          
-//             <div className='qwe1'>
-//                <EdItem />
-//             </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Home;
-
 import React from "react"
 import "../pages/Wrapper.css"
 import '../../App.css';

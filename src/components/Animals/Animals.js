10d778072970b9c0aa5c1f400c6e1074977f4601
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-12 11:02:34 +0530

    modified

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -1,16 +1,20 @@
 import React from "react"
 import Categories from "../pages/Categories"
 import SliderHome from "./Slider"
+import AnCategories from "./AnCategories"
 
 const Home = () => {
   return (
     <>
-      <section className='home'>
-      <Categories />
+      <div className='cow2'>
+        <Categories />
+      </div>
+      <div className='cow3'>
+        <AnCategories />
+      </div>
         <div className='container d_flex'>
           <SliderHome />
         </div>
-      </section>
     </>
   )
 }

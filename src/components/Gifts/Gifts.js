0295163aa7b04af4e6c72f88ae5b3df09ae1b5d7
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-18 09:34:25 +0530

    modified

--- src/components/Gifts/Gifts.js
+++ src/components/Gifts/Gifts.js
@@ -1,16 +1,20 @@
 import React from "react"
 import Categories from "../pages/Categories"
 import SliderHome from "./Slider"
+import GiCategories from "./GiCategories"
 
 const Home = () => {
   return (
     <>
-      <section className='home'>
-      <Categories />
+      <div className='cow2'>
+        <Categories />
+      </div>
+      <div className='cow3'>
+        <GiCategories />
+      </div>
         <div className='container d_flex'>
           <SliderHome />
         </div>
-      </section>
     </>
   )
 }

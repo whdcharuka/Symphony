75d6990efa8e41c4adfd29d45af7521ad5871008
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-28 17:45:56 +0530

    modified

--- src/components/Animals/Farm.js
+++ src/components/Animals/Farm.js
@@ -9,7 +9,7 @@ const Wrapper = () => {
   const data = [
     {
       path:"./Vp1",
-      cateImg: "./images/Animals/13.jpg",
+      cateImg: "./images/Animals/11.jpg",
       title: "Horn ",
       brand:"Apple",
       model:"single",
@@ -19,7 +19,7 @@ const Wrapper = () => {
     },
     {
       path:"./Vp2",
-      cateImg: "./images/Animals/14.jpg",
+      cateImg: "./images/Animals/12.jpg",
       title: "Air filter ",
       brand:"Apple",
       model:"single",
@@ -29,7 +29,7 @@ const Wrapper = () => {
     },
     {
       path:"./Vp3",
-      cateImg: "./images/Animals/15.jpg",
+      cateImg: "./images/Animals/13.jpg",
       title: "Fuel filter ",
       brand:"Apple",
       model:"single",
